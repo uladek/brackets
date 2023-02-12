1a851636cf472065c449bffb6987d3afff808401
Uladimir Hrydzin <hrydzin@gmail.com>
2023-02-12 20:53:37 +0100

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+//   if(str.length%2 !== 0) {
+//     return false;
+// }
+
+let target = bracketsConfig.map(el => el.join(''))
+for(let i in str) {
+// for(let i =-1; i < str.length; i++){
+    // console.log(str[i])
+// 
+    // for (let k=0; k< target.length; k++){
+        for(let t of target) {
+        while(str.includes(t)) {
+        str = str.replace(t ,'');
+    }
+ }
 }
+return str.length===0;
+// иначе false
+ 
+ }
+
+
